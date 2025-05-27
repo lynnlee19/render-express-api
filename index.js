e93db94d4b6df6db006e45374458fb1871c4313b
unknown <jameilee@yfp.local>
2025-05-27 09:29:28 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(cors());
+
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from Render Express API!" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
